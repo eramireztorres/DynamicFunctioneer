6851dccc3fec2dd953cdd825f7e5d1d5c32e08eb
eramireztorres <erickeduardoramireztorres@gmail.com>
2025-02-25 09:57:29 +0100

    Upgraded to version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dynamic_functioneer",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python package for dynamic function and method handling with LLM support.",
     author="Erick Eduardo Ramirez Torres",
     author_email="erickeduardoramireztorres@gmail.com",
